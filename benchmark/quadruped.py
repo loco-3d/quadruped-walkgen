a2396c7fbbc4665720f44557a9559dfd5f68e7d0
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-12-06 15:05:20 +0100

    [Format] black

--- benchmark/quadruped.py
+++ benchmark/quadruped.py
@@ -4,13 +4,14 @@ import numpy as np
 import time
 import matplotlib.pylab as plt
 from quadruped_walkgen import *
-import sys 
-
+import sys
 
 
 N = 16  # number of nodes
 T = int(sys.argv[1]) if (len(sys.argv) > 1) else int(5000)  # number of trials
-MAXITER = int(sys.argv[2]) if (len(sys.argv) > 1) else int(1)  # number of maximum iteration
+MAXITER = (
+    int(sys.argv[2]) if (len(sys.argv) > 1) else int(1)
+)  # number of maximum iteration
 
 # Gait Matrix
 gait = np.zeros((6, 5))
@@ -18,53 +19,129 @@ fsteps = np.full((6, 13), np.nan)
 
 # MpcInterface object that contains information about the current state of the robot
 # Change the initial conditions here
-lC = np.array([[0.0, 0.0, 0.2]]).transpose()  # CoM centered and at 20 cm above the ground
+lC = np.array(
+    [[0.0, 0.0, 0.2]]
+).transpose()  # CoM centered and at 20 cm above the ground
 abg = np.array([[0.0, 0.0, 0.0]]).transpose()  # horizontal base (roll, pitch, 0.0)
 lV = np.array([[0.2, 0.0, 0.0]]).transpose()  # motionless base (linear velocity)
 lW = np.array([[0.0, 0.0, 0.0]]).transpose()  # motionless base (angular velocity)
-l_feet = np.array([[0.19, 0.19, -0.19, -0.19],
-                                 [0.15005, -0.15005, 0.15005, -0.15005],
-                                 [0.0, 0.0, 0.0, 0.0]])  # position of feet in local frame
+l_feet = np.array(
+    [
+        [0.19, 0.19, -0.19, -0.19],
+        [0.15005, -0.15005, 0.15005, -0.15005],
+        [0.0, 0.0, 0.0, 0.0],
+    ]
+)  # position of feet in local frame
 
 
-x0 = np.vstack((lC, abg,lV, lW))  # Current state vector
+x0 = np.vstack((lC, abg, lV, lW))  # Current state vector
 # The reference state, copy of the initial position,
-xref = np.repeat(x0, N+1, axis=1)  # Desired future state vectors  
-xref[6,:] = 0.0   # Target linear velocity to Vx = 0 
+xref = np.repeat(x0, N + 1, axis=1)  # Desired future state vectors
+xref[6, :] = 0.0  # Target linear velocity to Vx = 0
 
 
-fsteps = np.array([[ 1.00000000e+00,  1.90776486e-01,  1.48962816e-01,  4.22498932e-03,
-   1.90060159e-01, -1.50265109e-01 , 0.00000000e+00 ,-1.89740429e-01,
-   1.50467686e-01 , 1.78713224e-06 ,-1.90692335e-01 ,-1.48946056e-01,
-   4.22561856e-03],
- [ 7.00000000e+00 , 1.90776486e-01 , 1.48962816e-01 , 4.22498932e-03,
-              np.nan ,             np.nan   ,           np.nan   ,           np.nan,
-               np.nan ,             np.nan ,-1.90692335e-01, -1.48946056e-01,
-   4.22561856e-03],
- [ 1.00000000e+00,  1.90776486e-01,  1.48962816e-01,  4.22498932e-03,
-   1.90000000e-01, -1.50050000e-01 , 0.00000000e+00 ,-1.90000000e-01,
-   1.50050000e-01,  0.00000000e+00, -1.90692335e-01, -1.48946056e-01,
-   4.22561856e-03],
- [ 7.00000000e+00  ,            np.nan  ,            np.nan       ,      np.nan,
-   1.90000000e-01 ,-1.50050000e-01,  0.00000000e+00, -1.90000000e-01,
-   1.50050000e-01,  0.00000000e+00 ,             np.nan      ,        np.nan,
-               np.nan],
- [ 0.00000000e+00  ,            np.nan ,             np.nan,              np.nan,
-               np.nan   ,           np.nan   ,  np.nan   ,           np.nan,
-               np.nan    ,          np.nan ,             np.nan         ,     np.nan,
-               np.nan],
- [ 0.00000000e+00   ,           np.nan     ,         np.nan      ,        np.nan,
-               np.nan         ,     np.nan       ,       np.nan     ,         np.nan,
-               np.nan        ,      np.nan,              np.nan ,             np.nan,
-               np.nan]])
+fsteps = np.array(
+    [
+        [
+            1.00000000e00,
+            1.90776486e-01,
+            1.48962816e-01,
+            4.22498932e-03,
+            1.90060159e-01,
+            -1.50265109e-01,
+            0.00000000e00,
+            -1.89740429e-01,
+            1.50467686e-01,
+            1.78713224e-06,
+            -1.90692335e-01,
+            -1.48946056e-01,
+            4.22561856e-03,
+        ],
+        [
+            7.00000000e00,
+            1.90776486e-01,
+            1.48962816e-01,
+            4.22498932e-03,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            -1.90692335e-01,
+            -1.48946056e-01,
+            4.22561856e-03,
+        ],
+        [
+            1.00000000e00,
+            1.90776486e-01,
+            1.48962816e-01,
+            4.22498932e-03,
+            1.90000000e-01,
+            -1.50050000e-01,
+            0.00000000e00,
+            -1.90000000e-01,
+            1.50050000e-01,
+            0.00000000e00,
+            -1.90692335e-01,
+            -1.48946056e-01,
+            4.22561856e-03,
+        ],
+        [
+            7.00000000e00,
+            np.nan,
+            np.nan,
+            np.nan,
+            1.90000000e-01,
+            -1.50050000e-01,
+            0.00000000e00,
+            -1.90000000e-01,
+            1.50050000e-01,
+            0.00000000e00,
+            np.nan,
+            np.nan,
+            np.nan,
+        ],
+        [
+            0.00000000e00,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+        ],
+        [
+            0.00000000e00,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+        ],
+    ]
+)
 
 
 def createProblem():
     # Cannot use 1 model for the whole control cycle, because each model depends on the position of the feet
     # And the inertia matrix depends on the reference state (approximation )
-    runningModels = [] 
+    runningModels = []
     x0 = np.zeros(12)
-    x0[2] = 0.2 
+    x0[2] = 0.2
     for i in range(N):
         model = ActionModelQuadruped()
 
@@ -72,61 +149,71 @@ def createProblem():
         runningModels.append(model)
 
     # Terminal Model
-    terminalModel = ActionModelQuadruped() 
+    terminalModel = ActionModelQuadruped()
 
-    terminalModel.forceWeights = np.zeros((12,1))
+    terminalModel.forceWeights = np.zeros((12, 1))
     terminalModel.frictionWeights = 0
 
-    problem = crocoddyl.ShootingProblem(x0,  runningModels, terminalModel)
+    problem = crocoddyl.ShootingProblem(x0, runningModels, terminalModel)
 
     xs = [x0] * (N + 1)
     us = [np.zeros(12)] * N
 
     return xs, us, problem
 
-def updateProblem(fsteps,xref , x0 , problem):
+
+def updateProblem(fsteps, xref, x0, problem):
     # Update the model according to the position of the feet in local frame (fstep)
-    #Construction of the gait matrix representing the feet in contact with the ground
-    index = next((idx for idx, val in np.ndenumerate(fsteps[:, 0]) if val==0.0), 0.0)[0]
+    # Construction of the gait matrix representing the feet in contact with the ground
+    index = next((idx for idx, val in np.ndenumerate(fsteps[:, 0]) if val == 0.0), 0.0)[
+        0
+    ]
     gait[:, 0] = fsteps[:, 0]
-    gait[:index, 1:] = 1.0 - (np.isnan(fsteps[:index, 1::3]) | (fsteps[:index, 1::3] == 0.0))
+    gait[:index, 1:] = 1.0 - (
+        np.isnan(fsteps[:index, 1::3]) | (fsteps[:index, 1::3] == 0.0)
+    )
 
     # Replace NaN values by zeroes
-    fsteps[np.isnan(fsteps)] = 0.0 
+    fsteps[np.isnan(fsteps)] = 0.0
     j = 0
     k_cum = 0
     L = []
-    
-    # Iterate over all phases of the gait
-    # The first column of xref correspond to the current state 
-    while (gait[j, 0] != 0):
-        for i in range(k_cum, k_cum+np.int(gait[j, 0])):
 
-            # Update model       
-            problem.runningModels[i].updateModel(np.reshape(fsteps[j, 1:], (3, 4), order='F') , xref[:, i+1] , gait[j, 1:])
-            
-            
+    # Iterate over all phases of the gait
+    # The first column of xref correspond to the current state
+    while gait[j, 0] != 0:
+        for i in range(k_cum, k_cum + np.int(gait[j, 0])):
+
+            # Update model
+            problem.runningModels[i].updateModel(
+                np.reshape(fsteps[j, 1:], (3, 4), order="F"),
+                xref[:, i + 1],
+                gait[j, 1:],
+            )
+
         k_cum += np.int(gait[j, 0])
         j += 1
 
-    
     # Update model of the terminal model
-    problem.terminalModel.updateModel(np.reshape(fsteps[j-1, 1:], (3, 4), order='F') , xref[:,-1] , gait[j-1, 1:]) 
-    
+    problem.terminalModel.updateModel(
+        np.reshape(fsteps[j - 1, 1:], (3, 4), order="F"), xref[:, -1], gait[j - 1, 1:]
+    )
+
     # update initial state of the problem
     problem.x0 = x0
     xs = [x0] * (N + 1)
     us = [np.zeros(12)] * N
 
-    return xs, us , problem 
+    return xs, us, problem
 
-def runUpdateProblemBenchmark(fsteps,xref , x0, problem):
+
+def runUpdateProblemBenchmark(fsteps, xref, x0, problem):
     xs = [x0] * (N + 1)
     us = [np.zeros(12)] * N
     duration = []
     for i in range(T):
         c_start = time.time()
-        xs, us, problem = updateProblem(fsteps,xref , x0 , problem)
+        xs, us, problem = updateProblem(fsteps, xref, x0, problem)
         c_end = time.time()
         duration.append(1e3 * (c_end - c_start))
 
@@ -134,6 +221,7 @@ def runUpdateProblemBenchmark(fsteps,xref , x0, problem):
     min_duration = min(duration)
     max_duration = max(duration)
     return avrg_duration, min_duration, max_duration
+
 
 def runDDPSolveBenchmark(xs, us, problem):
     ddp = crocoddyl.SolverDDP(problem)
@@ -149,6 +237,7 @@ def runDDPSolveBenchmark(xs, us, problem):
     max_duration = max(duration)
     return avrg_duration, min_duration, max_duration
 
+
 def runShootingProblemCalcBenchmark(xs, us, problem):
     duration = []
     for i in range(T):
@@ -161,6 +250,7 @@ def runShootingProblemCalcBenchmark(xs, us, problem):
     min_duration = min(duration)
     max_duration = max(duration)
     return avrg_duration, min_duration, max_duration
+
 
 def runShootingProblemCalcDiffBenchmark(xs, us, problem):
     duration = []
@@ -176,16 +266,34 @@ def runShootingProblemCalcDiffBenchmark(xs, us, problem):
     return avrg_duration, min_duration, max_duration
 
 
-    
-print('Python bindings:')
+print("Python bindings:")
 xs, us, problem = createProblem()
-xs, us, problem = updateProblem(fsteps,xref , x0 , problem)
-avrg_duration, min_duration, max_duration = runUpdateProblemBenchmark(fsteps,xref , x0 , problem)
-print('  UpdateModel [ms]: {0} ({1}, {2})'.format(avrg_duration, min_duration, max_duration))
+xs, us, problem = updateProblem(fsteps, xref, x0, problem)
+avrg_duration, min_duration, max_duration = runUpdateProblemBenchmark(
+    fsteps, xref, x0, problem
+)
+print(
+    "  UpdateModel [ms]: {0} ({1}, {2})".format(
+        avrg_duration, min_duration, max_duration
+    )
+)
 avrg_duration, min_duration, max_duration = runDDPSolveBenchmark(xs, us, problem)
-print('  DDP.solve [ms]: {0} ({1}, {2})'.format(avrg_duration, min_duration, max_duration))
-avrg_duration, min_duration, max_duration = runShootingProblemCalcBenchmark(xs, us, problem)
-print('  ShootingProblem.calc [ms]: {0} ({1}, {2})'.format(avrg_duration, min_duration, max_duration))
-avrg_duration, min_duration, max_duration = runShootingProblemCalcDiffBenchmark(xs, us, problem)
-print('  ShootingProblem.calcDiff [ms]: {0} ({1}, {2})'.format(avrg_duration, min_duration, max_duration))
-
+print(
+    "  DDP.solve [ms]: {0} ({1}, {2})".format(avrg_duration, min_duration, max_duration)
+)
+avrg_duration, min_duration, max_duration = runShootingProblemCalcBenchmark(
+    xs, us, problem
+)
+print(
+    "  ShootingProblem.calc [ms]: {0} ({1}, {2})".format(
+        avrg_duration, min_duration, max_duration
+    )
+)
+avrg_duration, min_duration, max_duration = runShootingProblemCalcDiffBenchmark(
+    xs, us, problem
+)
+print(
+    "  ShootingProblem.calcDiff [ms]: {0} ({1}, {2})".format(
+        avrg_duration, min_duration, max_duration
+    )
+)
--- exemple/GaitProblem.py
+++ exemple/GaitProblem.py
@@ -6,7 +6,6 @@ from quadruped_walkgen import *
 
 
 class GaitProblem:
-
     def __init__(self, dt=0.02, nx=12, nu=12, mu=0.8):
 
         # :param state: state description,
@@ -42,7 +41,7 @@ class GaitProblem:
         self.mu = mu
 
     def createProblem(self):
-        for i in range(int(self.T_mpc/self.dt)):
+        for i in range(int(self.T_mpc / self.dt)):
             model = ActionModelQuadruped()
 
             # Add model to the list of model
@@ -54,7 +53,9 @@ class GaitProblem:
         self.terminalModel.forceWeights = np.zeros((12, 1))
         self.terminalModel.frictionWeights = 0
 
-        self.problem = crocoddyl.ShootingProblem(np.zeros(12),  self.ListAction, self.terminalModel)
+        self.problem = crocoddyl.ShootingProblem(
+            np.zeros(12), self.ListAction, self.terminalModel
+        )
         self.ddp = crocoddyl.SolverDDP(self.problem)
 
     def updateProblem(self, fsteps, xref, x0):
@@ -68,22 +69,28 @@ class GaitProblem:
 
         # Iterate over all phases of the gait
         # The first column of xref correspond to the current state
-        while (self.gait[j, 0] != 0):
-            for i in range(k_cum, k_cum+np.int(self.gait[j, 0])):
+        while self.gait[j, 0] != 0:
+            for i in range(k_cum, k_cum + np.int(self.gait[j, 0])):
 
                 # Update model
-                self.ListAction[i].updateModel(np.reshape(self.fsteps[j, 1:], (3, 4),
-                                                          order='F'), xref[:, i+1], self.gait[j, 1:])
-                print(j, " ", i+1, " ", j)
+                self.ListAction[i].updateModel(
+                    np.reshape(self.fsteps[j, 1:], (3, 4), order="F"),
+                    xref[:, i + 1],
+                    self.gait[j, 1:],
+                )
+                print(j, " ", i + 1, " ", j)
 
             k_cum += np.int(self.gait[j, 0])
             j += 1
 
         print(self.gait)
-        
+
         # Update model of the terminal model
-        self.terminalModel.updateModel(np.reshape(
-            self.fsteps[j-1, 1:], (3, 4), order='F'), xref[:, -1], self.gait[j-1, 1:])
+        self.terminalModel.updateModel(
+            np.reshape(self.fsteps[j - 1, 1:], (3, 4), order="F"),
+            xref[:, -1],
+            self.gait[j - 1, 1:],
+        )
 
         # update initial state of the problem
         self.problem.x0 = x0
@@ -91,9 +98,13 @@ class GaitProblem:
     def createGaitMatrix(self):
 
         # Construction of the gait matrix representing the feet in contact with the ground
-        index = next((idx for idx, val in np.ndenumerate(self.fsteps[:, 0]) if val == 0.0), 0.0)[0]
+        index = next(
+            (idx for idx, val in np.ndenumerate(self.fsteps[:, 0]) if val == 0.0), 0.0
+        )[0]
         self.gait[:, 0] = self.fsteps[:, 0]
-        self.gait[:index, 1:] = 1.0 - (np.isnan(self.fsteps[:index, 1::3]) | (self.fsteps[:index, 1::3] == 0.0))
+        self.gait[:index, 1:] = 1.0 - (
+            np.isnan(self.fsteps[:index, 1::3]) | (self.fsteps[:index, 1::3] == 0.0)
+        )
 
         # Replace NaN values by zeroes
         self.fsteps[np.isnan(self.fsteps)] = 0.0
